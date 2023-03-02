7fcb5776bef34d1b98318360333de592f4bb906a
Ilya Mironov <mironov-i@bk.ru>
2023-03-02 22:17:50 +0700

    Add examples

---
+++ examples/search_min_elem_in_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+    srand(time(NULL));
+    const int max_rand = 100;
+    const int min_rand = 1;
+
+    const int size_array = 10;
+    int array[size_array];
+
+    for (int i = 0; i < size_array; ++i) {
+        array[i] = rand() % max_rand + min_rand;
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    int index_of_min_elem = 0;
+    for (int i = 1; i < size_array; ++i) {
+        if (array[i] < array[index_of_min_elem]) {
+            index_of_min_elem = i;
+        }
+    }
+
+    printf(
+        "Min elem:\narray[%d] = %d\n",
+        index_of_min_elem,
+        array[index_of_min_elem]);
+
+    return 0;
+}
